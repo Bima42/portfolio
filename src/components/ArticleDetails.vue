c45242981ada13afb7f35d4127ddf407fe4eb595
MattDirty <matt@le-dahu-studio.ch>
2022-11-09 23:21:08 +0100

    added barebones for pictures in projects

--- src/components/ArticleDetails.vue
+++ src/components/ArticleDetails.vue
@@ -1,10 +1,12 @@
 <template>
   <h1>{{article.title}}</h1>
   <p>{{article.description}}</p>
+  <ArticlePicturesCarousel :pictures='article.pictures'/>
 </template>
 
 <script lang="ts" setup>
 import { defineProps } from 'vue'
+import ArticlePicturesCarousel from '@/components/ArticlePicturesCarousel.vue'
 
 const props = defineProps({ article: {} })
 </script>
---
+++ src/components/ArticlePicturesCarousel.vue
@@ -0,0 +1,19 @@
+<template>
+  <template v-for='(picture, n) in pictures' v-bind:key="n">
+    <img :src="getImgSrc(picture)" alt="salut"/>
+  </template>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({ pictures: [] })
+
+function getImgSrc (image: string) {
+  return require('../assets/' + image)
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
