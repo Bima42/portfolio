efa4e264ae38c0c481ce05da37c3a2f6330b5f16
MattDirty <matt@le-dahu-studio.ch>
2022-11-09 22:58:52 +0100

    base

---
+++ src/components/ArticleDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{article.title}}</h1>
+  <p>{{article.description}}</p>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({ article: {} })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/ArticlesList.vue
@@ -0,0 +1,14 @@
+<template>
+  <section v-for="(article, n) in articlesData" v-bind:key="n">
+    <ArticleDetails :article="article"/>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import ArticleDetails from '@/components/ArticleDetails.vue'
+import articlesData from '@/assets/articles.json'
+</script>
+
+<style lang="scss" scoped>
+
+</style>
