c346b6b3fc67b93c03cea46f7dd514d1b6564e0f
Bima42 <tanpau42@gmail.com>
2025-07-25 00:08:51 +0200

    chore: simplify css config and basic color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,19 +5,6 @@ const config: Config = {
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
-      },
-      colors: {
-        primary: '#0066cc',
-        'primary-dark': '#66b3ff',
-      },
-    },
-  },
-  plugins: [],
 }
 
 export default config
