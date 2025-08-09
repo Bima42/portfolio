a58810980e942d6aa63b99aefe328e56d5bd3310
Bima42 <tanpau42@gmail.com>
2025-08-09 22:22:55 +0200

    fix: remove border in modal

--- src/components/projects/atoms/ProjectModal.tsx
+++ src/components/projects/atoms/ProjectModal.tsx
@@ -51,7 +51,7 @@ export function ProjectModal({
             )}
 
             <DialogContent
-                className="max-w-[95vw] w-full max-h-[90vh] overflow-hidden p-0 sm:max-w-[90vw] z-101"
+                className="max-w-[95vw] w-full max-h-[90vh] overflow-hidden p-0 sm:max-w-[90vw] z-101 border-none"
                 onWheel={e => e.stopPropagation()}
                 onTouchMove={e => e.stopPropagation()}
             >
