5a287c5de5ea1c5b46ce74f7bdbc6a606c326f5b
Bima42 <tanpau42@gmail.com>
2025-08-11 14:03:44 +0200

    chore: remove contact route

--- src/routes/contact.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import Contact from '../components/contact/Contact.tsx';
-
-export const Route = createFileRoute('/contact')({
-    component: Contact,
-});
