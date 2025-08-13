59fd5674f5df9900ac4210590ecad9c3ab6c9d7d
Bima42 <tanpau42@gmail.com>
2025-08-13 06:59:05 +0200

    chore: reverse timeline item order

--- src/components/about/atoms/CardHeader.tsx
+++ src/components/about/atoms/CardHeader.tsx
@@ -1,5 +1,6 @@
 import { Logo } from '@/components/header';
 import type { TimelineItem } from '@/components/about/types.ts';
+import { useLanguage } from '@/hooks/useLanguage.ts';
 
 interface CardHeaderProps {
     item: TimelineItem;
@@ -7,10 +8,11 @@ interface CardHeaderProps {
 }
 
 export function CardHeader({ item, getTypeIcon }: CardHeaderProps) {
+    const { t } = useLanguage();
     return (
         <div className="flex items-center justify-between mb-4">
             <span className="inline-block px-3 py-1 text-sm font-semibold text-primary bg-primary/10 rounded-full">
-                {item.date}
+                {t(item.date)}
             </span>
             {item.icon ? (
                 <Logo src={item.icon} alt={'card-logo'} logoHeight={'h-6'} />
