64e1d6ddffe4cf00f4ba964f5f9e587e759dd3eb
Bima42 <tanpau42@gmail.com>
2025-12-07 17:44:56 +0100

    feat: put back cv button

--- src/components/header/molecules/desktop/DesktopControls.tsx
+++ src/components/header/molecules/desktop/DesktopControls.tsx
@@ -2,19 +2,20 @@ import { VerticalDivider } from '../../atoms';
 import { LanguageToggle, ThemeToggle } from '../index.ts';
 import { GithubButton } from '@/components/header/molecules/icon-buttons/GithubButton.tsx';
 import { LinkedInButton } from '@/components/header/molecules/icon-buttons/LinkedInButton.tsx';
+import { CVButton } from '@/components/header/molecules/icon-buttons/CVButton.tsx';
 
 export function DesktopControls() {
     return (
         <div className="hidden md:flex items-center space-x-3">
             <VerticalDivider />
+
             <GithubButton />
-            <VerticalDivider />
             <LinkedInButton />
+            <CVButton />
+
             <VerticalDivider />
-            {/*<CVButton />*/}
-            {/*<VerticalDivider />*/}
+
             <LanguageToggle />
-            <VerticalDivider />
             <ThemeToggle />
         </div>
     );
--- src/components/header/molecules/mobile/MobileSocialButtons.tsx
+++ src/components/header/molecules/mobile/MobileSocialButtons.tsx
@@ -1,19 +1,14 @@
 import { GithubButton } from '@/components/header/molecules/icon-buttons/GithubButton.tsx';
 import { LinkedInButton } from '@/components/header/molecules/icon-buttons/LinkedInButton.tsx';
 import { useLanguage } from '@/hooks/useLanguage.ts';
+import { CVButton } from '@/components/header/molecules/icon-buttons/CVButton.tsx';
 
 export function MobileSocialButtons() {
     const { t } = useLanguage();
     return (
-        <div
-            className="
-                border-t border-foreground/10 border-foreground/20
-                bg-foreground/5 bg-foreground/10
-                p-6 space-y-4
-            "
-        >
+        <div className="border-t border-foreground/10 bg-foreground/5 p-6 space-y-4">
             <div className="text-center">
-                <span className="text-xs font-medium text-foreground/70 text-foreground/60 uppercase tracking-wider">
+                <span className="text-xs font-medium text-foreground/70 uppercase tracking-wider">
                     {t('header.connectAndDownload')}
                 </span>
             </div>
@@ -21,7 +16,7 @@ export function MobileSocialButtons() {
             <div className="flex flex-col justify-center space-y-4 px-6">
                 <GithubButton />
                 <LinkedInButton />
-                {/*<CVButton />*/}
+                <CVButton />
             </div>
         </div>
     );
